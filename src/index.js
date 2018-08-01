57f3c88ef23652f81dc5d72fc36d5bb361c7823c
ysz <1582945670@qq.com>
2018-08-02 00:09:42 +0800

    面向对象2-5

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert(100);
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    eat(){
+        alert(`${this.name} eat something`)
+    }
+    speak(){
+        alert(`My name is ${this.name}, age ${this.age}`)
+    }
+}
+let zhang = new Person('zhang', 20);
+zhang.eat();
+zhang.speak();
+
+let wang = new Person('wang', 21);
+wang.eat();
+wang.speak();
