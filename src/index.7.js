e7d1ebdc4c53bde405e77f944cef5dc22e6d8780
ysz <1582945670@qq.com>
2018-08-06 22:13:10 +0800

    单例模式

---
+++ src/index.7.js
@@ -0,0 +1,25 @@
+class SingleObject{
+    login(){
+        console.log('login...')
+    }
+}
+SingleObject.getInstance = (function(){
+    let instance;
+    return function(){
+        if(!instance){
+            instance = new SingleObject()
+        }
+        return instance
+    }
+})()
+
+let obj1 = SingleObject.getInstance()
+obj1.login()
+let obj2 = SingleObject.getInstance()
+obj2.login()
+console.log('obj1 ===obj2',obj1===obj2)
+
+console.log('---------分割线----------')
+let obj3 = new SingleObject() //无法完全控制
+obj3.login()
+console.log('obj1 === obj3', obj1 === obj3)
---
+++ src/index.js
@@ -0,0 +1,41 @@
+class LoginForm{
+    constructor(){
+        this.state = 'hide'
+    }
+    show(){
+        if(this.state === 'show'){
+            alert('已经显示')
+            return
+        }
+        this.state = 'show'
+        console.log('登录框显示成功')
+    }
+    hide(){
+        if(this.state === 'hide'){
+            alert('已经隐藏')
+            return
+        }
+        this.state = 'hide'
+        console.log('登录框隐藏成功')
+    }
+}
+
+LoginForm.getInstance = (function(){
+    let instance;
+    return function(){
+        console.log(instance)
+        if(!instance){
+            instance = new LoginForm()
+        }
+        return instance
+    }
+})()
+
+//测试
+let login1 = LoginForm.getInstance()
+login1.show()
+
+let login2 = LoginForm.getInstance()
+login2.show()
+
+console.log("login1 === login2", login1 === login2)
